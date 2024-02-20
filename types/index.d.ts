a48349ad6e8a5d2495130fffe7ec2fc174a559e5
Hariprasath <truebillionhari@gmail.com>
2024-02-20 22:18:45 +0530

    gyuu

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,5 @@
 declare type CreateUserParams = {
-   firstName: string; lastName: string; username: string; photo: string;
+  firstName: string; lastName: string; username: string; photo: string;
 }
 declare type UpdateUserParams = {
   firstName: string; lastName: string; username: string; photo: string;
